705da6492d9a47e9468c010051b7f3afcb64b477
Raphael Lee <gunjunese@gmail.com>
2023-02-12 20:30:16 +0900

    Problem 34. solved

---
+++ P34.py
@@ -0,0 +1,16 @@
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        first = 0
+        last = 0
+        for i in range(len(nums)):
+            if target == nums[i]:
+                result.append(i)
+
+        if len(result) == 0:
+            first, last = -1, -1
+        else:
+            first, last = result[0], result[-1]
+            
+        result = [first, last]
+
+        return result
